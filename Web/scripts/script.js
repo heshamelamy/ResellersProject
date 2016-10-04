47e18d4e390869d592fe9c1457d6a8ac28222bee
hesham <heshamelelamy@gmail.com>
2016-10-04 16:45:43 +0200

    Show Image Before Upload Working

--- Web/scripts/script.js
+++ Web/scripts/script.js
@@ -30,5 +30,23 @@ $(document).ready(function() {
 	$("i").click(function () {
     $('.search > input[type="text"],.search > .out > .fa').toggle();
 	$('.nav-left,.nav-right').toggle()("display","none");
-    });
+	});
+
+	$("#imgInp").change(function () {
+	    readURL(this);
+	});
 });
+
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
